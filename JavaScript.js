ac22e54a1883a9d5a3f97ed278dca6ee7dde7de4
Ivan P <tmf0@yandex.ru>
2016-12-02 17:27:54 +0300

    init 1

---
+++ JavaScript.js
@@ -0,0 +1,28 @@
+﻿function show(st) {
+    var mdiv = document.getElementById('mydiv');
+    mdiv.innerHTML = 'Привет ' + st + '!';
+}
+$(document).ready(function () {
+    $('#mydiv').text('Hello');
+})
+$(document).ready(function () {
+    $('.green').css('background', '#0000ff');
+})
+
+/*$(document).ready(function () {
+    $('#button').click(function () {
+        var text = $('#text').val();
+        alert(text);
+    })
+})
+$(document).ready(function () {
+    $('#button').click(function () {
+        $('#block').css('width', '100px');
+    })
+})*/
+$(document).ready(function () {
+    $('#button').click(function () {
+        var val = $('#block').children('p').text();
+        alert(val);
+    })
+})
